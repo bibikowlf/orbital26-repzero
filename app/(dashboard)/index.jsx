4d9139e6a1a408d3c0fff8e35450a94b9d3d912c
bibikowlf <bibikowolf@gmail.com>
2026-05-20 15:10:52 +0800

    Add user log-in and sign-up

---
+++ app/(dashboard)/index.jsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Image, View, Text } from 'react-native'
+import { Link } from 'expo-router'
+import { appStyles } from '../../styles/styles'
+import Spacer from '../../components/spacer'
+import Logo from '../../assets/RepZero_logo.png'
+
+const Home = () => {
+    const styles = appStyles
+
+    return (
+        <View style={styles.container}>
+            <Image source={Logo} style={styles.image} />
+
+            <Spacer />
+            
+            <Text>Welcome to</Text>
+
+            <Spacer height={10} />
+
+            <Text style={styles.title}>
+                RepZero
+            </Text>
+
+            <Spacer />
+
+            <Link href="/" style={styles.link}>  { /* add log out link*/ }
+                <Text>Log Out</Text>
+            </Link>
+        </View>
+    )
+}
+
+export default Home
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,27 +1,27 @@
-import { StyleSheet, Text, View, Image, useColorScheme } from 'react-native'
 import { Stack } from 'expo-router'
-import { Colors } from "../constants/Colors"
 import { StatusBar } from 'expo-status-bar'
+import { SplashScreenController } from '../components/splashscreen-controller'
+import { useAuthContext } from '../hooks/auth-context'
+import AuthProvider from '../providers/auth-provider'
 
-const RootLayout = () => {
-    const colorScheme = useColorScheme()
-    const theme = Colors[colorScheme] ?? Colors.light
-
-    return (
-        <>
-            <StatusBar value="auto" />
-            <Stack screenOptions={{
-                headerStyle: { backgroundColor: theme.navBackground },
-                headerTintColor: theme.title,
-            }}>
-                <Stack.Screen name="index" options={{ title: 'Home' }} />
-                <Stack.Screen name="about" options={{ title: 'About' }} />
-                <Stack.Screen name="contact" options={{ title: 'Contact' }} />
-            </Stack>
-        </>
-    )
+// Separate RootNavigator so we can access the AuthContext
+function RootNavigator() {
+  const { isLoggedIn } = useAuthContext()
+  return (
+    <Stack>
+      <Stack.Protected guard={isLoggedIn}>
+        <Stack.Screen name="(dashboard)/index" options={{ title: "Home" }}/>
+      </Stack.Protected>
+      <Stack.Screen name="(auth)/login" options={{ title: "Login" }} />
+    </Stack>
+  )
 }
-
-export default RootLayout
-
-const styles = StyleSheet.create({})
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <SplashScreenController />
+      <RootNavigator />
+      <StatusBar style="auto" />
+    </AuthProvider>
+  )
+}
