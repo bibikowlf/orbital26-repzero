b7877506b6a47627f0fdb65ef534db1c7481e720
bibikowlf <bibikowolf@gmail.com>
2026-06-15 09:42:40 +0800

    Add handleNumericInput function

---
+++ functions/numeric-input.js
@@ -0,0 +1,6 @@
+  export const handleNumericInput = (text) => {
+    const cleanedValue = text.replace(/[^0-9]/g, '')
+    const parsedValue = parseInt(cleanedValue, 10)
+
+    return isNaN(parsedValue) ? 0 : parsedValue
+  }
