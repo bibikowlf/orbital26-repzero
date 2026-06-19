8d233c11756e6390e0a10711cd94be4e8c92dd4a
bibikowlf <bibikowolf@gmail.com>
2026-06-19 10:42:30 +0800

    Add thread line for discussion forum

--- components/comment.jsx
+++ components/comment.jsx
@@ -8,7 +8,7 @@ import TextInfo from './text-info'
 export default function Comment({ comment, depth, editing, loading, onReplyPress, onVotePress, onEditPress, onUpdatePress, onDeletePress }) {
   const { claims } = useAuthContext()
   const userId = claims?.sub
-  const indentation = depth === 0 ? 0 : 16
+  const indentation = Math.min(depth, 4) === 0 ? 0 : 1
   const [editComment, setEditComment] = useState(null)
   const styles = appStyles
 
@@ -21,45 +21,53 @@ export default function Comment({ comment, depth, editing, loading, onReplyPress
   }, [editing])
 
   return (
-    <View style={{ marginLeft: indentation }}>
-      <Text style={{color: 'gray', fontSize: 12}}>
-        {comment.user_id === userId ? 'You': '@' + comment.username}
-      </Text>
-      {editing !== comment.id ? (
-        <Text style={{ fontSize: 16 }}>{comment.content}</Text>): (
-        <View>
-          <TextInput 
-            value={editComment}
-            onChangeText={(text) => setEditComment(text)}
-            autoCapitalize='none'
-            multiline={true}
-            textAlignVertical='top'
-            numberOfLines={10}
-            style={styles.input}
-          />
-          <TouchableOpacity
-            style={[styles.actionButton, 
-              { backgroundColor: '#007AFF', flex: 0, marginTop: 10 }]}
-            onPress={() => onUpdatePress(editComment)}
-            disabled={loading}
-          >
-            <Text style={styles.buttonText}>Edit</Text>
-          </TouchableOpacity>
-        </View>
-      )}
-      <TextInfo 
-        marginBottom={10}
-        isAuthor={comment.user_id === userId} 
-        canReply={true} 
-        score={comment.score} 
-        loading={loading}
-        editing={editing === comment.id}
-        hasVoted={comment.voted !== null} 
-        onVotePress={() => onVotePress(comment)} 
-        onEditPress={() => onEditPress(comment)} 
-        onDeletePress={() => onDeletePress(comment.id)} 
-        onReplyPress={() => onReplyPress(comment)}
-      />
+    <View style={{ 
+      borderLeftWidth: indentation * 2,
+      borderLeftColor: '#E2E8F0',
+      marginLeft: indentation * 8,
+      paddingLeft: indentation * 12,
+      marginTop: 6 }}
+    >
+      <View style={{flex:1}}>
+        <Text style={{color: 'gray', fontSize: 12}}>
+          {comment.user_id === userId ? 'You': '@' + comment.username}
+        </Text>
+        {editing !== comment.id ? (
+          <Text style={{ fontSize: 16 }}>{comment.content}</Text>): (
+          <View>
+            <TextInput 
+              value={editComment}
+              onChangeText={(text) => setEditComment(text)}
+              autoCapitalize='none'
+              multiline={true}
+              textAlignVertical='top'
+              numberOfLines={10}
+              style={styles.input}
+            />
+            <TouchableOpacity
+              style={[styles.actionButton, 
+                { backgroundColor: '#007AFF', flex: 0, marginTop: 10 }]}
+              onPress={() => onUpdatePress(editComment)}
+              disabled={loading}
+            >
+              <Text style={styles.buttonText}>Edit</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        <TextInfo 
+          marginBottom={10}
+          isAuthor={comment.user_id === userId} 
+          canReply={true} 
+          score={comment.score} 
+          loading={loading}
+          editing={editing === comment.id}
+          hasVoted={comment.voted !== null} 
+          onVotePress={() => onVotePress(comment)} 
+          onEditPress={() => onEditPress(comment)} 
+          onDeletePress={() => onDeletePress(comment.id)} 
+          onReplyPress={() => onReplyPress(comment)}
+        />
+      </View>
       {comment.replies && comment.replies.map(reply => (
         <Comment 
           key={reply.id} comment={reply} depth={depth + 1} 
