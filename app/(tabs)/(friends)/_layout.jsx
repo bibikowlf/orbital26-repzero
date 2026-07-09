54ca041781508775ea5a982030da76c733dac704
Aayushi <e1591061@u.nus.edu>
2026-07-09 09:54:48 +0800

    added back button in layout

--- app/(tabs)/(friends)/_layout.jsx
+++ app/(tabs)/(friends)/_layout.jsx
@@ -12,8 +12,11 @@ export default function FriendsLayout() {
       <Stack.Screen name='index' 
        options={{ title: 'Friends', headerLeft: () => <SettingsButton /> }}
        />
-      <Stack.Screen name="search-users" 
-        options={{ title: 'Find Friends', headerLeft: () => <BackButton /> }}
+      <Stack.Screen name='follow-list' 
+        options={({ route }) => ({
+          title: route.params?.mode === 'followers' ? 'Followers' : 'Following',
+          headerLeft: () => <BackButton />
+        })}
       />
     </Stack>
   )
