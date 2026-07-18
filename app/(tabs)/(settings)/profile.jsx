766a46dec62d8fd43388bd882605189ad36dba8a
bibikowlf <bibikowolf@gmail.com>
2026-07-18 20:14:59 +0800

    Delete change password buttons

--- app/(tabs)/(settings)/profile.jsx
+++ app/(tabs)/(settings)/profile.jsx
@@ -290,27 +290,6 @@ export default function Profile() {
         </View>
 
         <View style={styles.verticallySpaced}>
-          <Button 
-            title="Change Password (relative)" 
-            onPress={() => router.navigate('./change-password')} 
-          />
-        </View>
-
-        <View style={styles.verticallySpaced}>
-          <Button 
-            title="Change Password (absolute)" 
-            onPress={() => router.navigate('/(tabs)/(settings)/change-password')} 
-          />
-        </View>
-
-        <View style={styles.verticallySpaced}>
-          <Button 
-            title="Change Password (my-tutorials)" 
-            onPress={() => router.navigate('/my-tutorials')} 
-          />
-        </View>
-
-        <View style={styles.verticallySpaced}>
           <SignOutButton />
         </View>
         <Spacer />
